43f57362ee4ea1b2ecfe57c246b9d07e8c55fe18
Oat Desktop <piyapoom_r@hotmail.com>
2022-05-22 23:43:38 -0600

    Create FileMetaData class

---
+++ FilesTransferClient/FileMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTransferClient
+{
+    internal class FileMetaData
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExt { get; set; }
+
+        public int FilesTotal { get; set; }
+
+        public List<string> FileNames { get; set; }
+    }
+}
