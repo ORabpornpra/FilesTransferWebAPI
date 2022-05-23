baa4bf9fb10da47ef593f9ddd19fbc806d424c63
Oat Desktop <piyapoom_r@hotmail.com>
2022-05-23 00:06:57 -0600

    Add FileUtility class and test POST metadata file method

---
+++ FilesTransferClient/FileUtility.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WebAPIClient
+{
+    internal class FileUtility
+    {
+        public static FileMetaData TestMetaData()
+        {
+            FileMetaData fileMetaData = new FileMetaData()
+            {
+                FileName = "FileName Test",
+                FileExt = "FileExt Test",
+                FilesTotal = 5,
+                FileNames = new List<string> { "Chunk_1", "Chunk_2", "Chunk_3", "Chunk_4", "Chunk_5" }
+            };
+
+            return fileMetaData;
+        }
+    }
+}
