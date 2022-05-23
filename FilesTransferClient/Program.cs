783dbca43dd2f9d9471d6d60c47e72d1262ca152
Oat Desktop <piyapoom_r@hotmail.com>
2022-05-22 23:39:52 -0600

    Init FilesTransferClient and test GET method

---
+++ FilesTransferClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using System.Configuration;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string defaultContentType = @"application/json";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello OAT World");
+
+            await Program.GetFileMetaDataAsync();
+        }
+
+        private static async Task GetFileMetaDataAsync()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue(defaultContentType));
+
+            var stringTask = client.GetStringAsync(ConfigurationManager.AppSettings.Get("FileMetaDataGet"));
+
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+        }
+    }
+}
